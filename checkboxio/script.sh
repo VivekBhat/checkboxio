d60770e864ce72a85a8faea03ed57a56aeff44fe
Vivek Bhat <vivekbhat@live.com>
2017-04-17 21:45:18 -0400

    added script

--- checkboxio/script.sh
+++ checkboxio/script.sh
@@ -2,4 +2,4 @@
 pwd
 ls
 cd /var/lib/jenkins/workspace/check.io/checkboxio/
-ansible-playbook main.yml -i inventory
+ansible-playbook main.yml -i inventory --private-key /tmp/ssh6395982132731973147.key
