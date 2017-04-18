bbc95662fefd8a4989df0fc23ef71f17b2952c9c
Vivek Bhat <vivekbhat@live.com>
2017-04-17 22:53:16 -0400

    abc

--- checkboxio/script.sh
+++ checkboxio/script.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 pwd
 ls
-
 cd /var/lib/jenkins/workspace/check.io/checkboxio/
 ls
 rm -rf main.retry
 pwd
+ssh-add do
 ansible all -m ping -i inventory
 ansible-playbook main.yml -i inventory
